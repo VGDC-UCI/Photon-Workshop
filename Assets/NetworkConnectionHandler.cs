2d25153e1a0d96718135c640b84a0c503a407876
EPKgit <ericpaulkim@gmail.com>
2019-02-08 02:16:04 -0800

    init

---
+++ Assets/NetworkConnectionHandler.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkConnectionHandler : MonoBehaviourPunCallbacks
+{
+
+	void Awake()
+	{
+		PhotonNetwork.AutomaticallySyncScene = true;
+	}
+	void Start()
+	{
+		ConnectToNetwork();
+	}
+	private void ConnectToNetwork()
+	{
+		if(!PhotonNetwork.IsConnected)
+		{
+			PhotonNetwork.ConnectUsingSettings();
+		}
+	}
+	public void TryToJoinGame()
+	{
+		if(PhotonNetwork.IsConnected)
+		{
+			PhotonNetwork.JoinRandomRoom();
+		}
+	}
+
+	public override void OnConnectedToMaster()
+	{
+		Debug.Log("Connected To Master");
+	}
+
+	public override void OnDisconnected(DisconnectCause cause)
+	{
+		Debug.Log("DCed because " + cause);
+	}
+
+	public override void OnJoinRandomFailed(short returnCode, string message)
+	{
+		Debug.Log("failed to join random room");
+		PhotonNetwork.CreateRoom(null, new RoomOptions());
+	}
+
+	public override void OnJoinedRoom()
+	{
+		Debug.Log("succesfully joined room");
+		PhotonNetwork.LoadLevel("GameScene");
+	}
+}
---
+++ Assets/NetworkGameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+
+public class NetworkGameManager : MonoBehaviourPunCallbacks
+{
+	public static GameObject localPlayerInstance;
+
+	public GameObject playerPrefab;
+
+	void Start()
+	{
+		if(PhotonNetwork.IsMasterClient)
+		{
+			localPlayerInstance = Instantiate(playerPrefab, Vector3.left, Quaternion.identity);
+		}
+		else
+		{
+			localPlayerInstance = Instantiate(playerPrefab, Vector3.right, Quaternion.identity);
+		}
+	}
+}
