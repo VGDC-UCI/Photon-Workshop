563e134abbd0ad8cafd616782dc7a0e70679a0a3
EPKgit <ericpaulkim@gmail.com>
2019-02-15 00:39:42 -0800

    Finished part 2

---
+++ Assets/PlayerRender.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+
+public class PlayerRender : MonoBehaviourPun
+{
+	private SpriteRenderer sprite;
+	
+	void Awake()
+	{
+		sprite = GetComponent<SpriteRenderer>();
+	}
+
+	void Start()
+	{
+		if(photonView.IsMine)
+		{
+			sprite.color = Color.green;
+		}
+		else
+		{
+			sprite.color = Color.red;
+		}
+	}
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,39 @@ using UnityEngine;
 
 using Photon.Pun;
 
-public class PlayerMovement : MonoBehaviourPun
+public class PlayerMovement : MonoBehaviourPun, IPunObservable
 {
+	public float movementSpeed = 1f;
+
+	private Rigidbody2D rb;
+	private float lastRecieved;
+
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
 	void Update()
 	{
-		
+		if(!photonView.IsMine)
+		{
+			return;
+		}
+		float moveHorizontal = Input.GetAxisRaw("Horizontal");
+		float moveVertical = Input.GetAxisRaw("Vertical");
+		rb.velocity = new Vector2(moveHorizontal, moveVertical).normalized * movementSpeed;
+	}
+
+	public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		if(stream.IsWriting)
+		{
+			stream.SendNext(transform.position);
+		}
+		else
+		{
+			transform.position = (Vector3)stream.ReceiveNext();
+			Debug.Log(Time.time - lastRecieved);
+			lastRecieved = Time.time;
+		}
 	}
 }
